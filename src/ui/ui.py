41424a156e582d12d746e6546030590ab5e8dfa2
Juho Siitonen <juho_tuomas@msn.com>
2023-11-27 16:03:17 +0200

    added ui folder and basic setup in index.py

---
+++ src/ui/ui.py
@@ -0,0 +1,16 @@
+
+class Ui:
+    def __init__(self):
+        pass
+
+    def run(self):
+        self.menu()
+        
+        while True:
+            print("")
+            komento = input("komento: ")
+
+
+    def menu(self):
+        print("Menu")
+        print("1 Lisää bibTex viite")

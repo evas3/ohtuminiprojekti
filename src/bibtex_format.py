e50c244b4edc266d30c3f71120b96bfd36d203d0
evas3 <117542269+evas3@users.noreply.github.com>
2023-11-27 17:09:07 +0200

    Update bibtex_format.py

--- src/bibtex_format.py
+++ src/bibtex_format.py
@@ -3,4 +3,3 @@ def book(title, author, year, publisher, address):
     refrence = "@book{x,\nauthor = "+author+",\nyear = "+year+",\ntitle = "+title+",\npublisher = "+publisher+",\naddress = "+address+", \n}"
     return refrence
 
-print(book("a", "b", "c", "d", "e"))

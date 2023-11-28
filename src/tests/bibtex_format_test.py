d9caee59586e170f71813cac18a5e6bb2f6b23d6
Eva-Maria <evamaria.asplund@gmail.com>
2023-11-28 15:25:47 +0200

    bibtex_format test fix

--- src/tests/bibtex_format_test.py
+++ src/tests/bibtex_format_test.py
@@ -2,8 +2,11 @@ import unittest
 from bibtex_format import Bibtex
 
 class TestBibtex_format(unittest.TestCase):
+    def setUp(self):
+        self.bibtex_format = Bibtex()
+        
     def test_book(self):
-        test = book(key, author, title, publisher, 2023, address)
+        test = self.bibtex_format.book(key, author, title, publisher, 2023, address)
         right = """
 @Book{key,
   author    = {author},

0b6c50fa691a97f8f13e18d1db525c09582b09a8
mikaelri <mikael.rintala@outlook.com>
2023-11-28 15:25:22 +0200

    add ui test file

--- src/tests/ui_tests.py
+++ src/tests/ui_tests.py
@@ -4,4 +4,29 @@ from bibtex_format import Bibtex
 
 class BibtexStub:
     def book(self):
-        pass
+        return {
+            "key": "{1}",
+            "author": "{testi}",
+            "title": "{testi}",
+            "publisher": "{testi}",
+            "year": "{testi}",
+            "address": "{testi}"
+        }
+
+class TestUi(unittest.TestCase):
+    def setUp(self):
+        self.bibtex_stub = BibtexStub()
+        self.ui = Ui(self.bibtex_stub)
+
+    def test_book_citation_is_added_correctly(self):
+        key = "TestKey"
+        title = "TestTitle"
+        author = "TestAuthor"
+        year = "2023"
+        publisher = "TestPublisher"
+        address = "TestAddress"
+
+        function_citation = self.ui.add_book_citation(key, title, author, year, publisher, address)
+        stub_citation = self.bibtex_stub.book(key, title, author, year, publisher, address)
+
+        self.assertEqual(function_citation, stub_citation)

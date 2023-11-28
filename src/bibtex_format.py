18196acab86c706b6ed51750f64581c1593b1d5b
evas3 <117542269+evas3@users.noreply.github.com>
2023-11-28 14:34:48 +0200

    Update bibtex_format.py

--- src/bibtex_format.py
+++ src/bibtex_format.py
@@ -2,10 +2,10 @@
 
 def book(key, title, author, year, publisher, address):
     refrence = """
-    @Book{"""+str(key)+""",
-      author    = """+str(author)+""",
-      title     = """+str(title)+""",
-      publisher = """+str(publisher)+""",
-      year      = """+str(year)+"""
-    }"""
+@Book{"""+str(key)+""",
+  author    = """+str(author)+""",
+  title     = """+str(title)+""",
+  publisher = """+str(publisher)+""",
+  year      = """+str(year)+"""
+}"""
     return refrence

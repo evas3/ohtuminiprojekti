00c1ea901583af48cd3b3358ba3983f24cc00f22
mikaelri <mikael.rintala@outlook.com>
2023-11-28 15:29:57 +0200

    update book function placing

--- src/bibtex_format.py
+++ src/bibtex_format.py
@@ -3,13 +3,13 @@ class Bibtex:
     def __init__(self):
         pass
 
-def book(key, title, author, year, publisher, address):
-    refrence = """
-@Book{"""+str(key)+""",
-  author    = {"""+str(author)+"""},
-  title     = {"""+str(title)+"""},
-  publisher = {"""+str(publisher)+"""},
-  year      = {"""+str(year)+"""},
-  address   = {"""+str(address)+"""}
-}"""
-    return refrence
+    def book(key, title, author, year, publisher, address):
+        refrence = """
+    @Book{"""+str(key)+""",
+    author    = {"""+str(author)+"""},
+    title     = {"""+str(title)+"""},
+    publisher = {"""+str(publisher)+"""},
+    year      = {"""+str(year)+"""},
+    address   = {"""+str(address)+"""}
+    }"""
+        return refrence

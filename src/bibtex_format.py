5045069428237851a14031e4a5636a09512cb4f5
Eva-Maria <evamaria.asplund@gmail.com>
2023-11-28 14:31:35 +0200

    bibtex_format fix

--- src/bibtex_format.py
+++ src/bibtex_format.py
@@ -1,4 +1,16 @@
 
+<<<<<<< HEAD
 def book(title, author, year, publisher, address):
     refrence = "@book{x,\nauthor = "+author+",\nyear = "+year+",\ntitle = "+title+",\npublisher = "+publisher+",\naddress = "+address+", \n}"
     return refrence
+=======
+def book(key, title, author, year, publisher, address):
+    refrence = """
+    @Book{"""+str(key)+""",
+      author    = """+str(author)+""",
+      title     = """+str(title)+""",
+      publisher = """+str(publisher)+""",
+      year      = """+str(year)+"""
+    }"""
+    return refrence
+>>>>>>> cdb5409 (bibtex_format fix)

d91dd0749fd72810791868d1655011da734b57ca
Juho Siitonen <juho_tuomas@msn.com>
2023-11-28 16:23:50 +0200

    modified file writer to write in data folder

--- src/repositories/reference_writer.py
+++ src/repositories/reference_writer.py
@@ -1,11 +1,18 @@
+import os
+from settings import DIRNAME
 
 class ReferenceWriter:
     def __init__(self):
-        pass
+        self.filename = "references.bib"
+        self.data_file_path = ""
+
+    def new_filename(self, filename):
+        self.filename = filename
 
     def write(self, data):
+        self.data_file_path = os.path.join(DIRNAME, "data", self.filename)
         try:
-            with open('references.bib', 'a', encoding="utf-8") as f:
+            with open(self.data_file_path, 'a', encoding="utf-8") as f:
                 f.write(data)
             return True
         except UnicodeEncodeError:
---
+++ src/settings.py
@@ -0,0 +1,3 @@
+import os
+
+DIRNAME = os.path.dirname(__file__)

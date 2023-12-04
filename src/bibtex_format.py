a8bb510cae22d745bd946753ec820917e0d48875
Eva-Maria <evamaria.asplund@gmail.com>
2023-12-04 12:36:55 +0200

    added other ref types

--- src/bibtex_format.py
+++ src/bibtex_format.py
@@ -3,13 +3,39 @@ class Bibtex:
     def __init__(self):
         pass
 
+    def key(self, author, year):
+        pass
+        
     def book(self, key, title, author, year, publisher, address):
         refrence = """
-@Book{"""+str(key)+""",
+@Book{"""+key+""",
   author    = {"""+str(author)+"""},
   title     = {"""+str(title)+"""},
   publisher = {"""+str(publisher)+"""},
   year      = {"""+str(year)+"""},
   address   = {"""+str(address)+"""}
 }"""
+        return refrence
+
+    def article(self, author, title, journal, year, volume, pages):
+        refrence = """
+@article{"""+self.key(author, year)+""",
+  author  = {"""+str(author)+"""},
+  title   = {"""+str(title)+"""},
+  journal = {"""+str(journal)+"""},
+  year    = {"""+str(year)+"""},
+  volume  = {"""+str(volume)+"""},
+  pages   = {"""+str(pages)+"""}
+}"""
+        return refrence
+    
+    def iproceedings(self, author, title, booktitle, year):
+        refrence = """
+@inproceedings{"""+self.key(author, year)+""",
+    author    = {"""+str(author)+"""},
+    title     = {"""+str(title)+"""},
+    booktitle = {"""+str(booktitle)+"""},
+    year      = {"""+str(year)+"""},
+}
+"""
         return refrence

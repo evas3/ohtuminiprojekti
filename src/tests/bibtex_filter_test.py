79b5cb0cd135fd53aea40b995f7839085eca49e5
Juho Siitonen <juho_tuomas@msn.com>
2023-12-13 19:34:14 +0200

    added tests for bibtexfilter

---
+++ src/tests/bibtex_filter_test.py
@@ -0,0 +1,25 @@
+import unittest
+from services.bibtex_filter import BibtexFilter
+
+test_case1 = [
+'@Book{123,',
+'  author    = Testi,',
+'  title     = test,',
+'  publisher = Test,',
+'  year      = 2023,',
+'  address   = Test ',
+'}' ]
+
+class TestBibtex_filter(unittest.TestCase):
+    def setUp(self):
+        self.filter = BibtexFilter()
+        self.test_case1 = []
+        self.test_case1.append(test_case1)
+
+    def test_filter_finds_match(self):
+        result = self.filter.filter_by("author", "Testi", self.test_case1)
+        self.assertNotEqual(result, [])
+
+    def test_filter_does_not_find_match(self):
+        result = self.filter.filter_by("author", "Testaaja", self.test_case1)
+        self.assertEqual(result, [])
